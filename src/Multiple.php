7cbb83629eedfad64748a9a5865cfd786c8ec506
Matt Henderson <matt_henderson@sil.org>
2017-06-22 12:02:13 -0400

    Set up (empty) main file.

---
+++ src/Multiple.php
@@ -0,0 +1,10 @@
+<?php
+namespace Sil\IdpPw\PasswordStore\Multiple;
+
+use Exception;
+use Sil\IdpPw\Common\PasswordStore\PasswordStoreInterface;
+use yii\base\Component;
+
+class Multiple extends Component implements PasswordStoreInterface
+{
+}
